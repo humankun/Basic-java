3bc04843fe1bbd49f1c1a55b88cb83c5ae3e416b
humankun <amznprime5768@gmail.com>
2024-07-29 20:24:42 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,30 @@
+const student = {
+    fullName: "Kappa",
+    age: 18,
+    percenile: 95,
+    isQualified: true,
+};
+console.log(student.age);
+student["fullName"]="Kappa chan";
+console.log(student["fullName"]);
+
+
+const product = {
+    productName:"Parker jotter standard CT ball pen(Black)",
+    rating:"4 stars",
+    noOfReviews:7002,
+    finalPrice:270,
+    originalPrice:285,
+    discount:"5%"
+};
+console.log(product);
+
+const profile= {
+    fullName:"shradhakhapra",
+    posts:195,
+    followers:"569K",
+    following:4,
+    threads:"@shradhakhapra",
+    currentPosition:"Entrepreneur",
+    about:"Apna college|Ex-microsoft,DRDO,To educate someone is the highest privilege <3"
+}
